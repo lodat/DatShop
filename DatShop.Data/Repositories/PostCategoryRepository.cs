85f43e45fbbd6b1c6f6970522169291dca13d56e
DESKTOP-VLNPPFH\datpc <dn.tkagg@gmail.com>
2018-02-27 23:07:06 +0700

    Fix Repository and Add Post Service

---
+++ DatShop.Data/Repositories/PostCategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using DatShop.Data.Infrastructures;
+using DatShop.Model.Models;
+
+namespace DatShop.Data.Repostitories
+{
+    public interface IPostCategoryRepository : IRepository<PostCategory>
+    {
+
+    }
+    public class PostCategoryRepository : RepositoryBase<PostCategory>, IPostCategoryRepository
+    {
+        public PostCategoryRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
